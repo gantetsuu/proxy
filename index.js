549b366a98ac9a24e8b7a75ce72f8209ea41d12d
gantetsu <anime.gojo.live@gmail.com>
2024-03-07 09:56:13 +0530

    backup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const ytdl = require("ytdl-core");
 const app = express();
 const port = 3000;
-
 app.get("/youtube/:id", async (req, res) => {
   const videoId = req.params.id;
   const url = `https://www.youtube.com/watch?v=${videoId}`;
@@ -17,16 +16,23 @@ app.get("/youtube/:id", async (req, res) => {
     res.setHeader("Content-Length", format.contentLength);
     ytdl(url, { format: format })
       ?.pipe(res)
-      ?.on("error", (err) => {
-        console.error("Error streaming video:", err);
-        res.status(500).send("Error streaming video");
+      ?.on("error", (e) => {
+        throw new Error("Error streaming video:", e);
       });
-  } catch (err) {
-    console.error("Error fetching video info:", err);
-    res.status(500).send("Error fetching video info");
+  } catch (er) {
+    console.error("Error fetching video info:", er);
+    const data = await fetch(
+      `https://m3u8proxy-m3u8proxy.owiwfk.easypanel.host/youtube/${videoId}`
+    );
+    res.setHeader("Content-Type", "video/mp4");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${videoId}.mp4"`
+    );
+    res.setHeader("Content-Length", data.headers.get("Content-Length"));
+    res.status(200).send(data.body);
   }
 });
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
